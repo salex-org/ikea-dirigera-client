ef445a617279cc1df5c7df3720efb5df800a0823
Sascha Gärtner <sascha.gaertner@salex.org>
2025-12-28 18:41:20 +0100

    added delete commands

--- README.md
+++ README.md
@@ -9,26 +9,8 @@ The current version is work in progress that does not cover all functions and ha
 
 ## Install and use the library
 
+Tbd.
+
 ## Install and use the CLI
 
-### CLI Commands
-
-| Command   | Subcommand | Args   | Flags                                       | Description                                                  | Ready |
-|-----------|------------|--------|---------------------------------------------|--------------------------------------------------------------|-------|
-| list      | hubs       |        | --output <format>                           | Search hubs in the local network using mDNS                  | y     |
-|           | devices    |        | --output <format> --context <name>          | Lists all devices in the current context                     | y     |
-|           | rooms      |        | --output <format> --context <name>          | Lists all rooms in the current context                       | y     |
-|           | scenes     |        | --output <format> --context <name>          | Lists all scenes in the current context                      | y     |
-|           | users      |        | --output <format> --context <name>          | List all users in the current context                        | y     |
-|           | contexts   |        | --output <format>                           | List all contexts available in the config                    | y     |
-| set       | context    | <name> |                                             | Set the current context                                      | y     |
-| authorize |            | <ip>   | --port <port> --context <name> --no-context | Creates a new Token                                          | y     |
-| show      | token      |        | --context <name>                            | Shows the access token from the current or specified context | y     |
-|           | user       | <id>   | --output <format> --context <name>          | Shows information about the user                             | y     |
-|           | room       | <id>   | --output <format> --context <name>          | Shows information about the room                             | y     |
-|           | scene      | <id>   | --output <format> --context <name>          | Shows information about the scene                            | y     |
-|           | device     | <id>   | --output <format> --context <name>          | Shows information about the device                           | y     |
-| delete    | context    | <name> | --context <name>                            | Deletes the context and the user                             |       |
-|           | user       | <id>   | --context <name>                            | Deletes the user                                             |       |
-| curl      |            | <url>  | --context <name>                            | Calls the url in the current context                         | y     | 
-| listen    |            |        | --context <name>                            | Listens for events                                           | y     |
+Tbd.
--- cmd/delete.go
+++ cmd/delete.go
@@ -4,7 +4,11 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/zalando/go-keyring"
 )
 
 // deleteCmd represents the delete command
@@ -14,16 +18,85 @@ var deleteCmd = &cobra.Command{
 	Short:   "Remove the specified element from the CLI config or the IKEA DIRIGERA Hub",
 }
 
+// deleteContextCmd represents the delete context command
+var deleteContextCmd = &cobra.Command{
+	Use:     "context <name>",
+	Aliases: []string{"ctx", "c"},
+	Short:   "Remove the specified context from the CLI config and the related user from the IKEA DIRIGERA Hub",
+	Args:    cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		contextName := args[0]
+		context, found := appConfig.Contexts[contextName]
+		if !found {
+			return fmt.Errorf("unknown context: %s", contextName)
+		}
+		dirigeraClient := getDirigeraClient(context)
+		currentUser, err := dirigeraClient.GetCurrentUser()
+		if err != nil {
+			fmt.Printf("warning: could not get current user: %w", err)
+		}
+		err = dirigeraClient.DeleteUser(currentUser.ID)
+		if err != nil {
+			fmt.Printf("warning: could not delete user: %w", err)
+		}
+
+		delete(appConfig.Contexts, contextName)
+		viper.Set("contexts", appConfig.Contexts)
+
+		if appConfig.CurrentContext == contextName {
+			appConfig.CurrentContext = ""
+			viper.Set("current_context", appConfig.CurrentContext)
+		}
+
+		err = viper.WriteConfig()
+		if err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				if err := viper.SafeWriteConfig(); err != nil {
+					return fmt.Errorf("error writing config: %w", err)
+				}
+			} else {
+				return fmt.Errorf("error writing config: %w", err)
+			}
+		}
+
+		err = keyring.Delete(appName, contextName)
+		if err != nil {
+			return fmt.Errorf("error removing token from keyring: %w", err)
+		}
+		fmt.Printf("Deleted context %s and user %s\n", contextName, currentUser.ID)
+
+		return nil
+	},
+}
+
+// deleteUserCmd represents the delete user command
+var deleteUserCmd = &cobra.Command{
+	Use:     "user <id>",
+	Aliases: []string{"u"},
+	Short:   "Remove the specified user from the IKEA DIRIGERA Hub",
+	Args:    cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		userID := args[0]
+		usedContext, usedContextName, err := getContext(cmd)
+		if err != nil {
+			return fmt.Errorf("could not get context: %w", err)
+		}
+		dirigeraClient := getDirigeraClient(usedContext)
+		err = dirigeraClient.DeleteUser(userID)
+		if err != nil {
+			return fmt.Errorf("could not delete user %s in %s: %w", userID, usedContextName, err)
+		}
+		fmt.Printf("User %s deleted in %s\n", userID, usedContextName)
+
+		return nil
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(deleteCmd)
 
-	// Here you will define your flags and configuration settings.
+	deleteCmd.AddCommand(deleteContextCmd)
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	deleteCmd.AddCommand(deleteUserCmd)
+	deleteUserCmd.Flags().StringP("context", "c", "", "Defines the context to use")
 }
